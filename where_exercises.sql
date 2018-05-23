eaa8baff65955b453f4a91be43f4c612a3a926cb
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-23 15:28:34 -0500

    finished last part of where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,15 +2,20 @@ USE employees;
 
 SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name = 'Irena' AND gender = 'm'
+      OR first_name = 'vidya' AND gender = 'm'
+      OR first_name = 'maya' AND gender = 'm';
+SELECT *
+FROM employees
+WHERE last_name LIKE '%e' OR last_name LIKE 'e%';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE '%e' AND last_name LIKE 'e%';
 
 SELECT *
 FROM employees
-WHERE hire_date LIKE '199%';
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
@@ -18,4 +23,4 @@ WHERE birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
