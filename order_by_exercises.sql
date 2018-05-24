ee87d2cb6350a8a8e43bb60d281c4508ab4c3a94
Kelly Villalobos <kvdesign1978@gmail.com>
2018-05-24 08:44:24 -0500

    pusshing unpushed work

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -18,7 +18,8 @@ WHERE last_name LIKE '%e';
 SELECT *
 FROM employees
 WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25'
-ORDER BY birth_date LIKE '%-12-25' ASC, hire_date DESC ;
+ORDER BY birth_date ASC , hire_date DESC
+LIMIT 5 OFFSET 45;
 
 SELECT *
 FROM employees
